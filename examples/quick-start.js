441b622709402ebc6677f1f2c9837c8a43e79b63
Karim DRIDI <karim.dridi@gmail.com>
2012-11-29 00:36:54 +0100

    Minor bug: Segmentation fault on getinfo for a unfilled value

--- examples/quick-start.js
+++ examples/quick-start.js
@@ -1,23 +1,32 @@
 Curl = require('../index');
 options = {CONNECTTIMEOUT: 2};
+keys = ["EFFECTIVE_URL", "CONTENT_TYPE", "PRIVATE", "FTP_ENTRY_PATH", "REDIRECT_URL", "PRIMARY_IP", "RTSP_SESSION_ID", "LOCAL_IP"]
 curl = Curl.create(options)
 url = 'www.nodejs.org';
 curl(url,  function(err) {
-  console.info("\x1b[33meffetcive url: " + this.info('EFFECTIVE_URL') + "\x1b[0m");
+  self = this
+  keys.forEach(function(key) {
+    console.info("\x1b[33m" + key + ": [" + self.info(key) + "]\x1b[0m");
+  })
   console.info("body length: " + this.body.length);
   this.close()
 });
-
 curl = Curl.create(options)
 url = 'www.yahoo.com'
 curl(url, function(err) {
-  console.info("\x1b[33meffetcive url: " + this.info('EFFECTIVE_URL') + "\x1b[0m");
+  self = this
+  keys.forEach(function(key) {
+    console.info("\x1b[33m" + key + ": [" + self.info(key) + "]\x1b[0m");
+  })
   console.info("body length: " + this.body.length);
   this.close()
 });
-
 curl = Curl.create(options)
 curl('https://www.google.com', {VERBOSE: 1, RAW: 1}, function(err) {
-  console.info("\x1b[33meffetcive url: " + this.info('EFFECTIVE_URL') + "\x1b[0m");
+  self = this
+  keys.forEach(function(key) {
+    console.info("\x1b[33m" + key + ": [" + self.info(key) + "]\x1b[0m");
+  })
+  console.info("body length: " + this.body.length);
   this.close()
 });
