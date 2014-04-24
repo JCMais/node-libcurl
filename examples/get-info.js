8c1cb3a95d46d520f68d599af48ab9fb1cdc9edf
Jonathan Cardoso Machado <me@jonathancardoso.com>
2014-04-24 14:24:05 -0300

    Update get-info.js

--- examples/get-info.js
+++ examples/get-info.js
@@ -8,8 +8,6 @@ curl.setOpt( Curl.option.FOLLOWLOCATION, true );
 curl.setOpt( Curl.option.COOKIEFILE, "" ); //enable cookies
 curl.perform();
 
-//http://curl.haxx.se/docs/caextract.html
-
 curl.on( 'end', function() {
 
     for ( var infoName in Curl.info ) {
