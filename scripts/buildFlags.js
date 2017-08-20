83ab54ae557d078afdf7755a026b78bb2171913b
Jonathan Cardoso Machado <me@jonathancardoso.com>
2017-08-20 13:12:11 -0300

    Clean ourselves after install, heavily based (copied) from nodegit

---
+++ scripts/buildFlags.js
@@ -0,0 +1,21 @@
+var fs = require( 'fs' );
+var path = require( 'path' );
+
+var isGitRepo;
+
+// Proudly copied from https://github.com/nodegit/nodegit/blob/288ab93/lifecycleScripts/buildFlags.js
+
+try {
+    fs.statSync( path.join( __dirname, '..', '.git' ) );
+    isGitRepo = true;
+} catch ( e ) {
+    isGitRepo = false;
+}
+
+module.exports = {
+    debugBuild: !!process.env.BUILD_DEBUG,
+    isElectron: process.env.npm_config_runtime === 'electron',
+    isGitRepo: isGitRepo,
+    isNwjs: process.env.npm_config_runtime === 'node-webkit',
+    mustBuild: !!( isGitRepo || process.env.BUILD_DEBUG || process.env.BUILD_ONLY )
+};
---
+++ scripts/execPromise.js
@@ -0,0 +1,17 @@
+// Proudly copied from https://github.com/nodegit/nodegit/blob/288ab93/lifecycleScripts/execPromise.js
+var cp = require( 'child_process' );
+
+// We have to manually promisify this because at this is required in lifecycle
+// methods and we are not guaranteed that any 3rd party packages are installed
+// at this point
+module.exports = function ( command, opts ) {
+    return new Promise( function ( resolve, reject ) {
+        return cp.exec( command, opts, function ( err, result ) {
+            if ( err ) {
+                reject( err );
+            } else {
+                resolve( result );
+            }
+        });
+    });
+};
