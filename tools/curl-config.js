2131c45924e88bcad1a18cb7f614cf03aa1cc5af
Jonathan Cardoso Machado <me@jonathancardoso.com>
2019-04-01 09:17:27 -0300

    build: stuff [skip appveyor]

--- tools/curl-config.js
+++ tools/curl-config.js
@@ -1,4 +1,4 @@
-const { exec, execSync } = require('child_process')
+const { exec } = require('child_process')
 
 const { argv } = process
 
@@ -10,19 +10,6 @@ if (!argv[2]) {
 const arg = argv[2].trim()
 
 exec('curl-config ' + arg, function(error, stdout, stderr) {
-  console.error('That is what we got:')
-  console.error({
-    PATH: process.env.PATH,
-    PATH_exec: execSync('echo $PATH').toString('utf-8'),
-    which_curl_config: execSync('which curl-config').toString('utf-8'),
-    curl_config_libs: execSync('curl-config --libs').toString('utf-8'),
-    curl_config_static_libs: execSync('curl-config --static-libs').toString(
-      'utf-8'
-    ),
-    curl_config_cflags: execSync('curl-config --cflags').toString('utf-8'),
-  })
-  console.error(stdout)
-  process.exit(1)
   if (error != null) {
     console.error(
       'Could not run curl-config, please make sure libcurl dev package is installed.'
