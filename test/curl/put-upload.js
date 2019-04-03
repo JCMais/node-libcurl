1b021a0bc7ab0ceb2c90ce2248ec4355d053d9d3
Jonathan Cardoso Machado <me@jonathancardoso.com>
2019-04-02 23:19:50 -0300

    tests: remove console.logs from put-upload since it is now working [skip appveyor]

--- test/curl/put-upload.js
+++ test/curl/put-upload.js
@@ -42,14 +42,12 @@ function hashOfFile(file, cb) {
   hash.setEncoding('hex')
 
   fd.on('end', function() {
-    console.log('hashOfFile - end')
     hash.end()
 
     cb(null, hash.read())
   })
 
   fd.on('error', function(error) {
-    console.log('hashOfFile - error', error)
     cb(error)
   })
 
@@ -69,7 +67,6 @@ beforeEach(function(done) {
   //get a hash of given file so we can assert later
   // that the file sent is equals to the one we created.
   hashOfFile(fileName, function(error, hash) {
-    console.log('beforeEach - obtained hash', error, hash)
     fileHash = hash
     done(error)
   })
@@ -99,7 +96,6 @@ before(function(done) {
     fs.writeSync(fd, req.body, 0, req.body.length, 0)
     fs.closeSync(fd)
     hashOfFile(uploadLocation, function(error, hash) {
-      console.log('server - obtained hash', error, hash)
       if (error) {
         res.status(500).send(error.message)
       } else {
@@ -110,7 +106,6 @@ before(function(done) {
   })
 
   app.use(function(error, req, res, next) {
-    console.log('server - error', error)
     if (error.type !== 'request.aborted') {
       res.status(500).send(error.message)
     } else {
@@ -159,7 +154,6 @@ it('should upload data correctly using READFUNCTION callback option', function(d
   curl.setOpt(Curl.option.UPLOAD, true)
 
   curl.on('end', function(statusCode, body) {
-    console.log('curl - evt - end', statusCode, body)
     statusCode.should.be.equal(200)
     body.should.be.equal(fileHash)
 
@@ -167,7 +161,6 @@ it('should upload data correctly using READFUNCTION callback option', function(d
   })
 
   curl.on('error', function(err) {
-    console.log('curl - evt - error', err)
     done(err)
   })
 
@@ -177,10 +170,9 @@ it('should upload data correctly using READFUNCTION callback option', function(d
   stream.on('error', function(err) {
     done(err)
 
-    // make sure curl is not left in "waiting for data" state
-    console.log('stream - evt - error - cancelling readfunc')
-
     cancelRequested = true
+
+    // make sure curl is not left in "waiting for data" state
     if (isPaused) {
       isPaused = false
       curl.pause(CURLPAUSE_CONT)
@@ -188,11 +180,9 @@ it('should upload data correctly using READFUNCTION callback option', function(d
   })
 
   stream.on('readable', function() {
-    console.log('stream - evt - readable')
+    // resume curl to let it ask for available data
     if (isPaused) {
-      console.log('stream - evt - readable - readfunc was paused, resuming')
       isPaused = false
-      // resume curl to let it ask for available data
       curl.pause(CURLPAUSE_CONT)
     }
   })
@@ -200,21 +190,17 @@ it('should upload data correctly using READFUNCTION callback option', function(d
   // stream has no method to get this state
   var isEnded = false
   stream.on('end', function() {
-    console.log('stream - evt - ended')
     isEnded = true
 
     // resume curl to let it see there is no more data, just in case it was paused
-    curl.pause(CURLPAUSE_CONT)
+    if (isPaused) {
+      isPaused = false
+      curl.pause(CURLPAUSE_CONT)
+    }
   })
 
   curl.setOpt(Curl.option.READFUNCTION, function(targetBuffer) {
-    console.log('readfunction callback - start', {
-      cancelRequested,
-      isEnded,
-      isPaused,
-    })
     if (cancelRequested) {
-      console.log('readfunction callback - cancelling by request')
       return CURL_READFUNC_ABORT
     }
 
@@ -223,17 +209,14 @@ it('should upload data correctly using READFUNCTION callback option', function(d
 
     if (readBuffer === null) {
       if (isEnded) {
-        console.log('readfunction callback - returning 0 because it ended')
         return 0
       }
       // stream buffer was drained and we need to pause curl while waiting for new data
       isPaused = true
-      console.log('readfunction callback - pausing because stream was drained')
       return CURL_READFUNC_PAUSE
     }
 
     readBuffer.copy(targetBuffer)
-    console.log(`returning length of data sent: ${readBuffer.length}`)
     return readBuffer.length
   })
 
