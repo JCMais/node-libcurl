acbd1284563d9c5c2e3f5a13395d7d4d7abdf11f
Jonathan Cardoso Machado <me@jonathancardoso.com>
2020-04-10 19:33:04 -0300

    utils: add benchmarks

---
+++ benchmark/README.md
@@ -0,0 +1,45 @@
+## Benchmarks
+> Disclaimer: Those benchmarks are probably far from real world usage scenarios and should not be taken too seriouly before doing tests with your use-case in mind first.
+
+If you feel any of the code available is wrong, please open a PR with a suggested fix.
+
+The idea is just to hit some webpage  doing a GET request to `/` and receiving the result body back.
+
+For libraries that do not return the data as string, code to do that should be added, as is the case with Node.js `http.request`, `node-fetch` and `node-libcurl.Easy`.
+
+A local server can be started with `yarn start-server`.
+
+Benchmark can be started with `yarn start`.
+
+### Results
+#### Win64 i7-7700HQ 3.4GHz
+##### local server
+```
+node.js http.request - GET x 395 ops/sec ±3.92% (68 runs sampled)
+axios - GET x 328 ops/sec ±5.84% (64 runs sampled)
+superagent - GET x 410 ops/sec ±2.31% (75 runs sampled)
+request - GET x 405 ops/sec ±2.85% (75 runs sampled)
+fetch - GET x 392 ops/sec ±3.66% (70 runs sampled)
+node-libcurl curly - GET x 657 ops/sec ±1.78% (77 runs sampled)
+node-libcurl Curl - GET x 696 ops/sec ±1.66% (79 runs sampled)
+node-libcurl Curl - reusing instance - GET x 673 ops/sec ±2.17% (78 runs sampled)
+node-libcurl Easy - GET x 173 ops/sec ±12.30% (55 runs sampled)
+node-libcurl Easy - reusing instance - GET x 532 ops/sec ±5.42% (63 runs sampled)
+Fastest is node-libcurl Curl - GET
+Done in 61.27s
+```
+##### example.com
+```
+node.js http.request - GET x 3.62 ops/sec ±1.87% (22 runs sampled)
+axios - GET x 3.65 ops/sec ±2.60% (22 runs sampled)
+superagent - GET x 3.68 ops/sec ±1.20% (22 runs sampled)
+request - GET x 3.64 ops/sec ±1.73% (22 runs sampled)
+fetch - GET x 3.67 ops/sec ±1.26% (22 runs sampled)
+node-libcurl curly - GET x 7.16 ops/sec ±5.35% (39 runs sampled)
+node-libcurl Curl - GET x 7.30 ops/sec ±1.51% (39 runs sampled)
+node-libcurl Curl - reusing instance - GET x 7.25 ops/sec ±2.16% (38 runs sampled)
+node-libcurl Easy - GET x 3.60 ops/sec ±2.50% (13 runs sampled)
+node-libcurl Easy - reusing instance - GET x 3.62 ops/sec ±1.25% (22 runs sampled)
+Fastest is node-libcurl Curl - GET,node-libcurl Curl - reusing instance - GET,node-libcurl curly - GET
+Done in 64.24s.
+```
---
+++ benchmark/index.js
@@ -0,0 +1,181 @@
+const http = require('http')
+const { curly, Curl, Easy } = require('node-libcurl')
+const axios = require('axios')
+const superagent = require('superagent')
+const request = require('request')
+const fetch = require('node-fetch')
+
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || '8080'
+const URL = `http://${HOST}:${PORT}`
+
+axios.defaults.baseURL = URL
+
+const Benchmark = require('benchmark')
+const suite = new Benchmark.Suite()
+
+suite.add('node.js http.request - GET', {
+  defer: true,
+  fn: (defer) => {
+    http
+      .request({ host: HOST, port: PORT, path: '/' }, (res) => {
+        res.setEncoding('utf8')
+        let rawData = ''
+        res.on('data', (chunk) => {
+          rawData += chunk
+        })
+        res.on('end', () => {
+          defer.resolve()
+        })
+      })
+      .end()
+  },
+})
+
+suite.add('axios - GET', {
+  defer: true,
+  fn: (defer) => {
+    axios.get('/').then(() => defer.resolve())
+  },
+})
+
+suite.add('superagent - GET', {
+  defer: true,
+  fn: (defer) => {
+    superagent.get(URL).end(() => {
+      defer.resolve()
+    })
+  },
+})
+
+suite.add('request - GET', {
+  defer: true,
+  fn: (defer) => {
+    request(URL, () => defer.resolve())
+  },
+})
+
+suite.add('fetch - GET', {
+  defer: true,
+  fn: (defer) => {
+    fetch(URL)
+      .then((res) => res.text())
+      .then((_body) => defer.resolve())
+  },
+})
+
+suite.add('node-libcurl curly - GET', {
+  defer: true,
+  fn: (defer) => {
+    curly.get(URL).then((_result) => defer.resolve())
+  },
+})
+
+suite.add('node-libcurl Curl - GET', {
+  defer: true,
+  fn: (defer) => {
+    const curl = new Curl()
+    curl.setOpt('URL', URL)
+    const onEnd = () => {
+      curl.close()
+      defer.resolve()
+    }
+    const onError = (error) => {
+      curl.close()
+      throw error
+    }
+    curl.on('end', onEnd)
+    curl.on('error', onError)
+    curl.perform()
+  },
+})
+
+let curlReuse = null
+suite.add('node-libcurl Curl - reusing instance - GET', {
+  defer: true,
+  setup: () => {
+    curlReuse = new Curl()
+    curlReuse.setOpt('URL', URL)
+  },
+  fn: (defer) => {
+    const onEnd = () => {
+      curlReuse.off('end', onEnd)
+      curlReuse.off('error', onError)
+      defer.resolve()
+    }
+    const onError = (error) => {
+      curlReuse.off('end', onEnd)
+      curlReuse.off('error', onError)
+      curlReuse.close()
+      throw error
+    }
+    curlReuse.on('end', onEnd)
+    curlReuse.on('error', onError)
+    curlReuse.perform()
+  },
+  teardown: () => {
+    curlReuse.close()
+  },
+})
+
+suite.add('node-libcurl Easy - GET', {
+  fn: () => {
+    const easy = new Easy()
+    let headers = ''
+    let body = ''
+
+    easy.setOpt('URL', URL)
+    easy.setOpt('HEADERFUNCTION', (data, size, nmemb) => {
+      headers += data.toString('utf8')
+      return size * nmemb
+    })
+    easy.setOpt('WRITEFUNCTION', (data, size, nmemb) => {
+      body += data.toString('utf8')
+      return size * nmemb
+    })
+
+    easy.perform()
+    easy.close()
+  },
+})
+
+let easyReuse = null
+suite.add('node-libcurl Easy - reusing instance - GET', {
+  defer: true,
+  setup: () => {
+    easyReuse = new Easy()
+    easyReuse.setOpt('URL', URL)
+  },
+  fn: (defer) => {
+    let headers = ''
+    let body = ''
+    easyReuse.setOpt('URL', URL)
+    easyReuse.setOpt('HEADERFUNCTION', (data, size, nmemb) => {
+      headers += data.toString('utf8')
+      return size * nmemb
+    })
+    easyReuse.setOpt('WRITEFUNCTION', (data, size, nmemb) => {
+      body += data.toString('utf8')
+      return size * nmemb
+    })
+    easyReuse.perform()
+    defer.resolve()
+  },
+  teardown: () => {
+    easyReuse.close()
+  },
+})
+
+suite.on('complete', function (defer) {
+  console.log('Fastest is ' + this.filter('fastest').map('name'))
+})
+
+suite.on('cycle', function (event) {
+  console.log(String(event.target))
+})
+
+suite.on('error', function (error) {
+  console.error(error)
+})
+
+suite.run({ async: true })
