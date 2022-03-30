5be1939eeacd2d9429871002bb5c753155d4a405
John Chadwick <john.chadwick@konghq.com>
2022-03-30 10:36:24 -0400

    fix: re-introduce forward decl hack

--- src/sslctx.c
+++ src/sslctx.c
@@ -1,5 +1,12 @@
-
-#include <openssl/ssl.h>
+// OpenSSL declarations, to avoid needing to muck with the include path.
+#ifndef SSL_OP_LEGACY_SERVER_CONNECT
+#define SSL_OP_LEGACY_SERVER_CONNECT 0x00000004U
+#endif
+#ifndef SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION
+#define SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION 0x00040000U
+#endif
+typedef struct ssl_ctx_st SSL_CTX;
+unsigned long SSL_CTX_set_options(SSL_CTX* ctx, unsigned long op);
 
 void node_libcurl_ssl_ctx_set_legacy_opts(void* sslctx) {
   SSL_CTX_set_options(sslctx, SSL_OP_LEGACY_SERVER_CONNECT);
