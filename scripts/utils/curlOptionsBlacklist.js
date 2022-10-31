f200465c5ce95d2293815e926b7d03ff5a524231
Jonathan Cardoso Machado <me@jonathancardoso.com>
2022-10-31 10:18:01 -0300

    feat: add missing CURLOPT_SERVER_RESPONSE_TIMEOUT

--- scripts/utils/curlOptionsBlacklist.js
+++ scripts/utils/curlOptionsBlacklist.js
@@ -3,7 +3,6 @@ const curlOptionsBlacklist = [
   // to add next
   'CURLOPT_PROTOCOLS_STR',
   'CURLOPT_REDIR_PROTOCOLS_STR',
-  'CURLOPT_SERVER_RESPONSE_TIMEOUT',
   'CURLOPT_SSH_HOST_PUBLIC_KEY_SHA256',
   'CURLOPT_SSH_HOSTKEYFUNCTION',
   // ws related options - to be added later(?)
