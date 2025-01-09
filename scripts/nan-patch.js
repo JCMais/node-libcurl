8ed95c782becb08262e121e5471f1d92c2586b6f
Filipe Freire <livrofubia@gmail.com>
2025-01-09 18:57:09 +0000

    fix: nan-patch edit for insomnia repo (#79)

--- scripts/nan-patch.js
+++ scripts/nan-patch.js
@@ -2,6 +2,11 @@
 const path = require('path')
 const fs = require('fs')
 const filePath = path.join(__dirname, '../node_modules/nan/nan.h')
+// if file not found, exit script without error (e.g. running `npm i` in insomnia repository)
+if (!fs.existsSync(filePath)) {
+  console.log('nan.h not found, skipping patch')
+  process.exit(0)
+}
 const fileContent = fs.readFileSync(filePath, 'utf8')
 const updatedContent = fileContent.replace(
   /#include "nan_scriptorigin.h"/,
